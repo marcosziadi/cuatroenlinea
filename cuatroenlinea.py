17e4c16e8ce9db8120924c4c65fb52a0313c8479
marcosziadi <139mziadi@gmail.com>
2021-04-13 10:57:28 -0300

    primercommit

---
+++ cuatroenlinea.py
@@ -0,0 +1,34 @@
+
+def tablerovacio():
+    return[
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ]
+
+
+def soltarfichaencolumna(ficha, columna, tablero):
+    for row in range(6, 0, -1):
+        if tablero[row - 1][int(columna) - 1] == 0:
+            tablero[row - 1][int(columna) - 1] = ficha
+            return
+
+
+tablero = tablerovacio()
+for row in tablero:
+       print(row)
+secuencia = 4
+
+for a in int(secuencia):
+    if a % 2 == 0:
+        ficha = 1
+    else:
+        ficha = 2
+    columna = input("\nInsertar Numero de Columna: ")
+
+    soltarfichaencolumna(ficha, columna, tablero)
+    for row in tablero:
+        print(row)
